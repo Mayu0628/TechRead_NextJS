f842491ba95e67c7bab418ab0e7ecdd0a50b6bdd
Mayu0628 <c1101427@planet.kanazawa-it.ac.jp>
2024-05-29 23:44:14 +0900

    chore: Update sidebar module CSS and component styles

--- src/app/components/layouts/Sidebar/Sidebar.tsx
+++ src/app/components/layouts/Sidebar/Sidebar.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import DescriptionIcon from "@mui/icons-material/Description";
 import SettingsIcon from "@mui/icons-material/Settings";
 import CalendarIcon from "@/features/Event/Calendar/CalendarIcon";
+import styles from "./Sidebar.module.css";
 
 const Sidebar = () => {
   return (
-    <div className="background">
-      <div className="icon">
+    <div className={styles.background}>
+      <div className={styles.icon}>
         <CalendarIcon />
         <DescriptionIcon />
       </div>

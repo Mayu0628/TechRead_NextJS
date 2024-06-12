673f6fc66e72aa9542b57801c5734258388090e9
Mayu0628 <c1101427@planet.kanazawa-it.ac.jp>
2024-06-12 18:40:25 +0900

    feat: Update ChapterList styles for better readability and user experience

--- src/app/features/Toppage/ChapterList/component/ChapterList.tsx
+++ src/app/features/Toppage/ChapterList/component/ChapterList.tsx
@@ -1,13 +1,14 @@
 import styles from "@/features/Toppage/ChapterList/component/ChapterList.module.css";
+import "@/styles/globals.css";
 
 const ChapterList = () => {
   return (
     <div className={styles.chapterlistcontainer}>
-      <h2>議事録一覧</h2>
+      <h3>議事録一覧</h3>
       <div className="">
         <ul>
-          <li>hogehoge</li>
-          <li>hogehoge</li>
+          <li># hogehoge</li>
+          <li># hogehoge</li>
         </ul>
       </div>
     </div>

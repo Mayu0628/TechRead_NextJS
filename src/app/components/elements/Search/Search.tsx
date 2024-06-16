8dccaee10a1692a842194bc20dcc182a102acde4
Mayu0628 <c1101427@planet.kanazawa-it.ac.jp>
2024-06-16 23:11:41 +0900

    feat: Remove unused prop from Search component

--- src/app/components/elements/Search/Search.tsx
+++ src/app/components/elements/Search/Search.tsx
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 
 import styles from "@/components/elements/Search/Search.module.css";
 
-const Search = ({ onSearch }) => {
+const Search = ({  }) => {
   const [query, setQuery] = useState("");
 
   const handleInputChange = (e) => {

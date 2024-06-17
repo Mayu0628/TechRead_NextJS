1fe6ebf10240c5ec2e7982df7542200d04debef2
Mayu0628 <c1101427@planet.kanazawa-it.ac.jp>
2024-06-17 23:21:39 +0900

    feat: Add search form to the main page and update global styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
 import "@/styles/globals.css";
-import Search from "@/components/elements/Search/Search";
 import BookList from "@/features/Toppage/BookList/component/BookList";
 import MinutesMain from "@/features/Toppage/Minutes/MinutesMain";
 
 export default function Home() {
   return (
-    <main className="maincontainer">
-      <BookList />
-      <Search onSearch={handleSearch} />
-      <MinutesMain />
+    <main>
+      <form action="#">
+        <input type="text" />
+      </form>
+      <div className="maincontainer">
+        <BookList />
+        <MinutesMain />
+      </div>
     </main>
   );
 }
